fabec3bfc2c126ebf5e564656975a276d152c01a
Валерий Чирков <zx84058405@live.com>
2022-12-10 17:02:13 +0300

    add task 4 final

--- src/task4.java
+++ src/task4.java
@@ -3,20 +3,18 @@
  */
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class task4 {
     public static void main(String[] args) {
-        ArrayList<ArrayList<String>> gameField = new ArrayList<>();
-        gameField = getGameField();
+        ArrayList<ArrayList<String>> gameField = getGameField();
         gameField.forEach(System.out :: println);
-        ArrayList<ArrayList<String>> queenONField = new ArrayList<>();
-        queenONField = getGameFieldQueenOn(gameField);
-        System.out.println();
+        ArrayList<ArrayList<String>> queenONField = getGameFieldQueenOn(gameField);
+        System.out.println("\n Королева будет 'W'.");
         queenONField.forEach(System.out::println );
-
-
+        System.out.println(
+                "\nВ данной задаче не было цели решить все возможные варианты данной задачи, " +
+                        "потому для отображения одного из вариантов, я применил зеркальное присвоение позиции " +
+                        "с проверкой на доступность данной операции.");
     }
     private static ArrayList<ArrayList<String>> getGameField (){
         ArrayList<ArrayList<String>> multiArray = new ArrayList<>();
@@ -24,7 +22,7 @@ public class task4 {
         String[] abc = new String[] {"A", "B", "C", "D", "E", "F", "G", "H"};
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                unoArray.add(abc[j] + String.valueOf(i + 1));
+                unoArray.add(abc[j] + (i + 1));
             }
             multiArray.add(unoArray);
             unoArray = new ArrayList<>();
@@ -34,8 +32,7 @@ public class task4 {
 
     private static ArrayList<ArrayList<String>> getGameFieldQueenOn ( ArrayList<ArrayList<String>> arrayList){
         String queen = "W";
-        Map<Integer, Integer> history = new HashMap<>();
-        while (true){
+
             int i = 0;
             while ( i < arrayList.size()/2) {
                 int j = 0;
@@ -43,10 +40,8 @@ public class task4 {
                     if(i == 0 || i == 1 || i == 2){
                         if(j >= arrayList.size()/2 - 1){
                             if (isEmptyPosition(arrayList, i, j)){
-                                history.put(i, j);
                                 arrayList.get(i).set(j, queen);
                                 if (isEmptyPosition(arrayList, arrayList.size() - 1 -i, arrayList.size() - 1 -j)){
-                                    history.put(arrayList.size() - 1 -i, arrayList.size() - 1 - j);
                                     arrayList.get(7-i).set(arrayList.size() - 1 -j, queen);
                                 }
                                 break;
@@ -54,10 +49,8 @@ public class task4 {
                         }
                     } else {
                         if (isEmptyPosition(arrayList, i, j)){
-                            history.put(i, j);
                             arrayList.get(i).set(j, queen);
                             if (isEmptyPosition(arrayList, arrayList.size() - 1 -i, arrayList.size() - 1 -j)){
-                                history.put(arrayList.size() - 1-i, arrayList.size() - 1 - j);
                                 arrayList.get(arrayList.size() - 1 -i).set(arrayList.size() - 1- j, queen);
                             }
                             break;
@@ -66,11 +59,7 @@ public class task4 {
                     j++;
                 }
                 i++;
-            System.out.println();
-            arrayList.forEach(System.out::println);
             }
-            break;
-        }
         return arrayList;
     }
     private static boolean isEmptyPosition (ArrayList<ArrayList<String>> arrayList, int rows, int columns ){
